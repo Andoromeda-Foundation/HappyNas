ffdd4aa5435bda9796ac9655fd4fe2fd57828d86
Frank Wei <frank@frankwei.xyz>
2018-09-16 00:18:25 +0800

    refactor: remove unused `Operator` class

--- Contracts/Nebulas/dice.js
+++ Contracts/Nebulas/dice.js
@@ -15,33 +15,6 @@ const BigNumberStorageDescriptor = {
         return o.toString(10);
     }
 }
-class Operator {
-    constructor(obj) {
-        this.operator = {}
-        this.parse(obj)
-    }
-
-    toString() {
-        return JSON.stringify(this.operator)
-    }
-
-    parse(obj) {
-        if (typeof obj != "undefined") {
-            var data = JSON.parse(obj)
-            for (var key in data) {
-                this.operator[key] = data[key]
-            }
-        }
-    }
-
-    get(key) {
-        return this.operator[key]
-    }
-
-    set(key, value) {
-        this.operator[key] = value
-    }
-}
 
 class OwnerableContract {
     constructor() {
